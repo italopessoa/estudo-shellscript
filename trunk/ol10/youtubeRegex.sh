14d6de3d03092cdeaf4c48288132c466b6125789
italoneypessoa@gmail.com <italoneypessoa@gmail.com@d7f6d8ef-c938-5a14-8f5f-f4f0d25edd48>
2012-09-15 17:28:59 +0000

    alteração incorreta

--- trunk/ol10/youtubeRegex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# youtubeRegex.sh
-# script simples para remover dados desnecessários da
-# url dos vídeos do youtube
-# "Italo Pessoa" <italoneypessoa@gmail.com>
-youtubeRegex(){
-	echo "$@" | sed 's/?.*[^v]v=/?v=/g; s/&.*//g'
-}
