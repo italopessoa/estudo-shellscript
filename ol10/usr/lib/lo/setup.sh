8b2c189cd5e3c69962a5a0e2dde276fe25e74174
italoneypessoa <italoneypessoa@gmail.com>
2012-09-25 03:12:35 +0000

    adicionado lancador adicionado icone adicinada nova opcao para passar direto para configuracão

--- ol10/usr/lib/lo/setup.sh
+++ ol10/usr/lib/lo/setup.sh
@@ -114,11 +114,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"
+along with this program.  If not, see <http://www.gnu.org/licenses/>."
 		;;
 		"--verbose")
 			echo "Esta opção não é utilzada sozinha."
+		;;
+		"-c" | "--configure" )
+			linkorganizer_configMenu
+			clear
 		;;
 		*)
 			echo "Opção desconhecida."
